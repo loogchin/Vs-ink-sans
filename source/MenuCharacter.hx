7c60df1d675d08171cc6ebc934db9223c8a3cf1e
loogchin <87599145+loogchin@users.noreply.github.com>
2021-09-04 13:53:21 +0700

    Add files via upload

---
+++ source/MenuCharacter.hx
@@ -0,0 +1,64 @@
+package;
+
+import flixel.FlxSprite;
+import flixel.graphics.frames.FlxAtlasFrames;
+
+class CharacterSetting
+{
+	public var x(default, null):Int;
+	public var y(default, null):Int;
+	public var scale(default, null):Float;
+	public var flipped(default, null):Bool;
+
+	public function new(x:Int = 0, y:Int = 0, scale:Float = 1.0, flipped:Bool = false)
+	{
+		this.x = x;
+		this.y = y;
+		this.scale = scale;
+		this.flipped = flipped;
+	}
+}
+
+class MenuCharacter extends FlxSprite
+{
+	private static var settings:Map<String, CharacterSetting> = [
+		'ink' => new CharacterSetting(-445, 13, 2.0)
+	];
+
+	private var flipped:Bool = false;
+
+	public function new(x:Int, y:Int, scale:Float, flipped:Bool)
+	{
+		super(x, y);
+		this.flipped = flipped;
+
+		antialiasing = true;
+
+		frames = Paths.getSparrowAtlas('ink menu', 'ink');
+
+		animation.addByPrefix('ink', "ink menu", 24);
+
+		setGraphicSize(Std.int(width * scale));
+		updateHitbox();
+	}
+
+	public function setCharacter(character:String):Void
+	{
+		if (character == '')
+		{
+			visible = false;
+			return;
+		}
+		else
+		{
+			visible = true;
+		}
+
+		animation.play(character);
+
+		var setting:CharacterSetting = settings[character];
+		offset.set(setting.x, setting.y);
+		setGraphicSize(Std.int(width * setting.scale));
+		flipX = setting.flipped != flipped;
+	}
+}
